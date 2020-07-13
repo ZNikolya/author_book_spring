faf4faf7ce533b1811bf0b3b3d881862dbf17e22
Admin <nikozohrabyan@mail.ru>
2020-07-13 20:26:50 +0400

    add

--- src/main/java/am/itspace/controller/AuthorController.java
+++ src/main/java/am/itspace/controller/AuthorController.java
@@ -4,18 +4,10 @@ package am.itspace.controller;
 import am.itspace.model.Author;
 import am.itspace.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 @Controller
